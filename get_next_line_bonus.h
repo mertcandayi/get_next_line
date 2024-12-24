8c5190eae0ae1f7be2299c36f835b92692082ce5
Mertcan <mrtcn15.dy@gmail.com>
2024-12-24 20:36:38 +0300

    bonus eklemesi

---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: medayi    <medayi@student.42istanbul>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/24 20:29:11 by medayi            #+#    #+#             */
+/*   Updated: 2024/12/24 20:33:19 by medayi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*read_line(int fd, char *str);
+char	*write_line(const char *str);
+char	*re_line(char *str);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *str);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: medayi    <medayi@student.42istanbul>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/24 20:29:11 by medayi            #+#    #+#             */
+/*   Updated: 2024/12/24 20:33:19 by medayi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*ptr;
+
+	ptr = (char *)s;
+	while (*ptr)
+	{
+		if ((char)c == *ptr)
+			return (ptr);
+		ptr++;
+	}
+	if ((char)c == 0)
+		return (ptr);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	i = 0;
+	d = (unsigned char *)dst;
+	s = (const unsigned char *)src;
+	if (!dst && !src)
+		return (0);
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dst);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	len;
+	char	*des;
+
+	len = ft_strlen(s) + 1;
+	des = malloc(sizeof(char) * len);
+	if (!des)
+		return (NULL);
+	return (ft_memcpy(des, s, len));
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*result;
+
+	if (!s1 || !s2)
+		return (0);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	result = (char *)malloc(len1 + len2 + 1);
+	if (!result)
+		return (0);
+	ft_memcpy(result, s1, len1);
+	ft_memcpy(result + len1, s2, len2);
+	result[len1 + len2] = '\0';
+	free(s1);
+	return (result);
+}
